ed874e0970ffbb3387589d8c74f8d7bc58bdf0af
Vinay Meti <vinaygouda.meti16@gmail.com>
2024-03-23 14:17:44 +0530

    Home import in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,24 @@
+import { BrowserRouter } from "react-router-dom";
+import MainRoute from "./MainRoute/MainRoute";
+import { Provider } from "react-redux";
+import store from "./Strore/Store";
+import { ChakraProvider } from "@chakra-ui/react";
+import NavBar from "./Components/NavBar";
+
 function App() {
-  return <>APP</>;
+  return (
+    <>
+      <ChakraProvider>
+        <Provider store={store}>
+          <BrowserRouter>
+            {/* <NavBar /> */}
+            <MainRoute />
+          </BrowserRouter>
+        </Provider>
+      </ChakraProvider>
+      ;
+    </>
+  );
 }
 
 export default App;
